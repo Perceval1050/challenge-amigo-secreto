62a4e7c6c523bb2f6ab299bc1163f3f25a64ef86
Perceval1050 <tecsistemas1.50@gmail.com>
2025-09-20 18:11:02 -0500

    actualiza-app.js_agregar nombres

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ let ListaDeAmigos = [];
 let VisualizarLista = document.getElementById("listaAmigos");
 
 function agregarAmigo() {
+    //Agregar nombres
     ListaDeAmigos.push(Nombres.value);
     console.log(ListaDeAmigos);
+    VisualizarLista.innerHTML += `<li>${Nombres.value}</li>`
+    
 };
 
