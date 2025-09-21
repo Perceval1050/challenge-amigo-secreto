960c6ded2143e0452cbd29f5c888b3c38ae06d55
Perceval1050 <tecsistemas1.50@gmail.com>
2025-09-20 19:20:24 -0500

    Actualización app.js

--- app.js
+++ app.js
@@ -2,12 +2,26 @@
 let Nombres = document.getElementById("amigo");
 let ListaDeAmigos = [];
 let VisualizarLista = document.getElementById("listaAmigos");
+let ResultadoDeSorteo = document.getElementById("resultado");
 
 function agregarAmigo() {
+    //vallidar entrada
+    if (!Nombres.value) {
+        alert("ingrese un nombre por favor");
+    }
     //Agregar nombres
     ListaDeAmigos.push(Nombres.value);
     console.log(ListaDeAmigos);
     VisualizarLista.innerHTML += `<li>${Nombres.value}</li>`
     
 };
+
+function sortearAmigo() {
+    //Sortiar y elejir amigo secreto
+    let sorteo = Math.floor(Math.random()* ListaDeAmigos.length);
+    let amigoSecreto = ListaDeAmigos[sorteo];
+    ResultadoDeSorteo.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+    }
+
+
 
